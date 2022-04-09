93e90b2ef4c38b0dfe6d19f3d6127ef4ca0dd0d0
tashfiaislamistila <tashfia.cse@gmail.com>
2022-04-08 18:13:16 -0700

    use configaration commit

--- src/Components/Login/Login.js
+++ src/Components/Login/Login.js
@@ -1,13 +1,17 @@
+import { getAuth } from 'firebase/auth';
 import React from 'react';
-import useFirebase from '../../hooks/useFirebase';
+import { useSignInWithGoogle } from 'react-firebase-hooks/auth'
+import app from '../../firebase.init';
+
+const auth = getAuth(app);
 
 const Login = () => {
-    const { signInWithGoogle } = useFirebase();
+    const [signInWithGoogle, user] = useSignInWithGoogle();
     return (
         <div>
             <h2>Please Login</h2>
             <div style={{ margin: "20px" }}>
-                <button onClick={signInWithGoogle}>Google Sign In</button>
+                <button onClick={() => signInWithGoogle()}>Google Sign In</button>
             </div>
             <br />
             <from>
