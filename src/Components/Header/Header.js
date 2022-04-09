b90fafb4ae5ccbecbd626329b0c05ef7bbdef51e
tashfiaislamistila <tashfia.cse@gmail.com>
2022-04-08 18:42:48 -0700

    use configaration use in different component commit

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,9 +1,13 @@
+import { getAuth, signOut } from 'firebase/auth';
 import React from 'react';
+import { useAuthState } from 'react-firebase-hooks/auth';
 import { Link } from 'react-router-dom';
-import useFirebase from '../../hooks/useFirebase';
+import app from '../../firebase.init';
 import './Header.css'
+
+const auth = getAuth(app);
 const Header = () => {
-    const { user, handleSignOut } = useFirebase();
+    const [user,] = useAuthState(auth);
     return (
         <div className='header'>
             <nav>
@@ -14,7 +18,7 @@ const Header = () => {
                 <span>{user?.displayName && user.displayName}</span>
                 {
                     user?.uid ?
-                        <button onClick={handleSignOut}>sign out</button>
+                        <button onClick={() => signOut(auth)}>sign out</button>
                         :
                         <Link to='/login'>Login</Link>
                 }
